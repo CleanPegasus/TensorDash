8b8f19d95ba180859350e2db48951537285d29fb
Harshit Maheshwari <harshitmah98@gmail.com>
2020-01-13 19:53:35 +0530

    Remove CreateAPIKeyActivity, Update Login Activity

--- Android App/app/src/main/java/com/example/tensordash/view/ui/LoginActivity.java
+++ Android App/app/src/main/java/com/example/tensordash/view/ui/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.tensordash.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.tensordash.R;
 
@@ -13,5 +16,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.button_create_new_key);
+        button.setOnClickListener(v->{
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity   .class);
+            startActivity(intent);
+        });
+
     }
 }
