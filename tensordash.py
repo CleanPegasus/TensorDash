d937655c56e7926c3113599060c7a55a9b71c3d4
CleanPegasus <arunk609@gmail.com>
2020-01-27 19:14:22 +0530

    Made the code cleaner

--- tensordash.py
+++ tensordash.py
@@ -1,84 +1,57 @@
 import requests
 import json
 import keras
-
 class FirebaseError(Exception):
     pass
-
 class SendDataToFirebase(object):
 
     def __init__(self, key = None):
-
         response = None
 
     def sendMessage(self, key = None, params = None, ModelName = 'Sample Model'):
-
         epoch, loss, acc, val_loss, val_acc = params
-
         data = '{"Epoch":' +  str(epoch+1) + ', "Loss" :' + str(loss) + ', "Accuracy" :' + str(acc) + ', "Validation Loss":' + str(val_loss) + ', "Validation Accuracy" :' + str(val_acc) + '}'
-
         response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data=data)
 
     def updateRunningStatus(self, key = None, ModelName = 'Sample Model'):
-
         data = '{"Status" : "RUNNING"}'
-
         response = requests.put('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data = data)
 
     def updateCompletedStatus(self, key = None, ModelName = 'Sample Model'):
-
         data = '{"Status" : "COMPLETED"}'
-
         response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data = data)
 
     def crashAnalytics(self, key = None, ModelName = 'Sample Model'):
-
         data = '{"Status" : "CRASHED"}'
-
         response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data = data)
 
-
-
 SendData = SendDataToFirebase()
-
-
 class Tensordash(keras.callbacks.Callback):
 
     def __init__(self, email = 'None', password = 'None',  ModelName = 'Sample_model'):
 
-        
         self.ModelName = ModelName
         self.email = email
         self.password = password
 
-
         headers = {'Content-Type': 'application/json',}
-
         params = (('key', 'AIzaSyDU4zqFpa92Jf64nYdgzT8u2oJfENn-2f8'),)
-
         val = {
             "email" : self.email,
             "password": self.password,
             "returnSecureToken": "false"
         }
-
         data = str(val)
 
         try:
-
             response = requests.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword', headers=headers, params=params, data=data)
-
             output = response.json()
-
             self.key = output['localId']
 
         except:
-
             raise FirebaseError("Authentication Failed. Kindly create an account on the companion app")
-
     
     def on_train_begin(self, logs = {}):
-
         self.losses = []
         self.accuracy = []
         self.val_losses = []
@@ -86,7 +59,6 @@ class Tensordash(keras.callbacks.Callback):
         self.num_epochs = []
 
         SendData.updateRunningStatus(key = self.key, ModelName = self.ModelName)
-
 
     def on_epoch_end(self, epoch, logs = {}):
 
@@ -96,7 +68,6 @@ class Tensordash(keras.callbacks.Callback):
         self.val_accuracy.append(logs.get('val_accuracy'))
         self.num_epochs.append(epoch)
 
-        
         self.loss = float("{0:.6f}".format(self.losses[-1]))
 
         if self.accuracy[-1] == None:
@@ -115,13 +86,10 @@ class Tensordash(keras.callbacks.Callback):
             self.val_acc = float("{0:.6f}".format(self.val_accuracy[-1]))
     
         values = [epoch, self.loss, self.acc, self.val_loss, self.val_acc]
-
         SendData.sendMessage(key = self.key, params = values, ModelName = self.ModelName)
 
     def on_train_end(self, epoch, logs = {}):
-
         SendData.updateCompletedStatus(key = self.key, ModelName = self.ModelName)
 
     def sendCrash(self):
-
         SendData.crashAnalytics(key = self.key, ModelName = self.ModelName)
