bfa4c860746b081b9e08b68ff67c484e8f74e9d0
Harshit Maheshwari <harshitmah98@gmail.com>
2020-01-28 19:02:29 +0530

    Add Signout feature

--- Android App/app/src/main/java/com/example/tensordash/view/ui/DashboardActivity.java
+++ Android App/app/src/main/java/com/example/tensordash/view/ui/DashboardActivity.java
@@ -1,35 +1,29 @@
 package com.example.tensordash.view.ui;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.tensordash.R;
-import com.example.tensordash.service.model.Project;
-import com.example.tensordash.service.repository.FirebaseDatabaseRepository;
 import com.example.tensordash.view.adapter.ProjectAdapter;
+import com.example.tensordash.viewmodel.FirebaseAuthViewModel;
+import com.example.tensordash.viewmodel.FirebaseAuthViewModelFactory;
 import com.example.tensordash.viewmodel.FirebaseDatabaseViewModel;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
 
     private static final String TAG = "DashboardActivity";
     FirebaseDatabaseViewModel databaseViewModel;
+    FirebaseAuthViewModel firebaseAuthViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +36,8 @@ public class DashboardActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(projectAdapter);
 
+        firebaseAuthViewModel = ViewModelProviders.of(DashboardActivity.this, new FirebaseAuthViewModelFactory(getApplication(), DashboardActivity.this)).get(FirebaseAuthViewModel.class);
+
         databaseViewModel = ViewModelProviders.of(DashboardActivity.this).get(FirebaseDatabaseViewModel.class);
         databaseViewModel.getAllProjects().observe(this, projects -> projectAdapter.submitList(projects));
 
@@ -51,4 +47,33 @@ public class DashboardActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_signout) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(DashboardActivity.this);
+            builder.setTitle("Sign out?")
+                    .setMessage("Do you want to sign out?")
+                    .setPositiveButton("Yes", (dialog, which) -> {signOut();})
+                    .setNegativeButton("No", (dialog, which) -> {})
+                    .create()
+                    .show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut(){
+        // TODO: Add a listener
+        firebaseAuthViewModel.signOut();
+        startActivity(new Intent(DashboardActivity.this, LoginActivity.class));
+        finishAffinity();
+    }
+
 }
