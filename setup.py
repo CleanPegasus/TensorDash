7873532d8c65af4ae383fbe533f0bc4df16d9ec9
CleanPegasus <arunk609@gmail.com>
2020-02-14 00:50:04 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ def readme():
 
 setup(
 
-    name = "tensordash",
-    version = "1.0",
+    name = "tensor-dash",
+    version = "1.3",
     description = "A python package that lets you remotely monitor your deep learning training metrics through it's companion app.",
     long_description = readme(),
     long_description_content_type = "text/markdown",
-    url = "https://github.com/CleanPegasus/TemsorDash",
+    url = "https://github.com/CleanPegasus/TensorDash",
     authors = "Harshit Maheshwari, Arunkumar L",
     author_email = "hmhmrock@gmail.com, arunk609@gmail.com",
     license = "MIT",
@@ -23,4 +23,5 @@ setup(
     ],
     packages = ["tensordash"],
     include_package_data = True,
-    install_requires = ["keras>=2.2.0", "json", "requests"]
+    install_requires = ["keras>=2.2.0", "requests"]
+    )
