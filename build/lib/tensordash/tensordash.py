9c746e47e774d2392614af150692dd89ca45f88a
CleanPegasus <arunk609@gmail.com>
2020-02-19 02:06:04 +0530

    Version 1.4.1

--- build/lib/tensordash/tensordash.py
+++ build/lib/tensordash/tensordash.py
@@ -8,7 +8,7 @@ class SendDataToFirebase(object):
     def __init__(self, key = None):
         response = None
 
-    def sendMessage(self, key = None, params = None, ModelName = 'Sample Model'):
+    def sendMessage(self, key = None, auth_token = None, params = None, ModelName = 'Sample Model'):
         epoch, loss, acc, val_loss, val_acc = params
         
         if(acc == None and val_loss == None):
@@ -20,32 +20,30 @@ class SendDataToFirebase(object):
         else:
             data = '{"Epoch":' +  str(epoch+1) + ', "Loss" :' + str(loss) + ', "Accuracy" :' + str(acc) + ', "Validation Loss":' + str(val_loss) + ', "Validation Accuracy" :' + str(val_acc) + '}'
 
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data=data)
+        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json?'.format(key, ModelName), params = auth_token, data=data)
 
-    def updateRunningStatus(self, key = None, ModelName = 'Sample Model'):
+    def updateRunningStatus(self, key = None, auth_token = None, ModelName = 'Sample Model'):
         data = '{"Status" : "RUNNING"}'
-        response = requests.put('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data = data)
+        response = requests.put('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
 
         notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Running"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', data = notif_data)
+        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
 
-    def updateCompletedStatus(self, key = None, ModelName = 'Sample Model'):
+    def updateCompletedStatus(self, key = None, auth_token = None, ModelName = 'Sample Model'):
         data = '{"Status" : "COMPLETED"}'
-        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data = data)
+        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
 
 
-        notif_data = notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Completed"}'
-        #notif_data = '{"Key" : "Sample Key", "Status" : "Running"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', data = notif_data)
+        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Completed"}'
+        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
 
-    def crashAnalytics(self, key = None, ModelName = 'Sample Model'):
+    def crashAnalytics(self, key = None, auth_token = None, ModelName = 'Sample Model'):
         data = '{"Status" : "CRASHED"}'
-        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/{}/{}.json'.format(key, ModelName), data = data)
+        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
 
 
         notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Crashed"}'
-        #notif_data = '{"Key" : "Sample Key", "Status" : "Running"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', data = notif_data)
+        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
 
 SendData = SendDataToFirebase()
 class Tensordash(keras.callbacks.Callback):
@@ -61,7 +59,7 @@ class Tensordash(keras.callbacks.Callback):
         val = {
             "email" : self.email,
             "password": self.password,
-            "returnSecureToken": "false"
+            "returnSecureToken": "true"
         }
         data = str(val)
 
@@ -69,6 +67,9 @@ class Tensordash(keras.callbacks.Callback):
             response = requests.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword', headers=headers, params=params, data=data)
             output = response.json()
             self.key = output['localId']
+            self.token = output['idToken']
+
+            self.auth_token = (('auth', self.token),)
 
         except:
             raise FirebaseError("Authentication Failed. Kindly create an account on the companion app")
@@ -80,13 +81,22 @@ class Tensordash(keras.callbacks.Callback):
         self.val_accuracy = []
         self.num_epochs = []
 
-        SendData.updateRunningStatus(key = self.key, ModelName = self.ModelName)
-        SendData.sendMessage(key = self.key, params = (-1, 0, 0, 0, 0), ModelName = self.ModelName)
+        SendData.updateRunningStatus(key = self.key, auth_token = self.auth_token, ModelName = self.ModelName)
+        SendData.sendMessage(key = self.key, auth_token = self.auth_token, params = (-1, 0, 0, 0, 0), ModelName = self.ModelName)
+    """
+    def on_epoch_begin(self, epoch, logs = {}):
+
+        self.filename = self.ModelName + '.h5'
+        self.model.save(self.filename, overwrite = True)
+    """
 
     def on_epoch_end(self, epoch, logs = {}):
 
         self.losses.append(logs.get('loss'))
-        self.accuracy.append(logs.get('accuracy'))
+        if(logs.get('acc') != None):
+            self.accuracy.append(logs.get('acc'))
+        else:
+            self.accuracy.append(logs.get('accuracy'))
         self.val_losses.append(logs.get('val_loss'))
         self.val_accuracy.append(logs.get('val_accuracy'))
         self.num_epochs.append(epoch)
@@ -109,11 +119,11 @@ class Tensordash(keras.callbacks.Callback):
             self.val_acc = float("{0:.6f}".format(self.val_accuracy[-1]))
     
         values = [epoch, self.loss, self.acc, self.val_loss, self.val_acc]
-        SendData.sendMessage(key = self.key, params = values, ModelName = self.ModelName)
+        SendData.sendMessage(key = self.key, auth_token = self.auth_token, params = values, ModelName = self.ModelName)
 
     def on_train_end(self, epoch, logs = {}):
 
-        SendData.updateCompletedStatus(key = self.key, ModelName = self.ModelName)
+        SendData.updateCompletedStatus(key = self.key, auth_token = self.auth_token, ModelName = self.ModelName)
 
     def sendCrash(self):
-        SendData.crashAnalytics(key = self.key, ModelName = self.ModelName)
+        SendData.crashAnalytics(key = self.key, auth_token = self.auth_token, ModelName = self.ModelName)
