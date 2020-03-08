99c69949fc97eb7fb7714d72b5904eabfdab42ff
pateldevang <devangpatel92@gmail.com>
2020-03-08 15:31:31 +0530

    splash screen done and fonts added.

---
+++ iOS App/TensorDash/TensorDash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TensorDash
+//
+//  Created by Devang Patel on 08/03/20.
+//  Copyright © 2020 Tensor Dash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
