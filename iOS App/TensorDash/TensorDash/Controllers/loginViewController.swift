ce18832a8e0c4ae0866b02439e538f208f99b7d6
pateldevang <devangpatel92@gmail.com>
2020-03-12 10:35:08 +0530

    Firebase setup done.

--- iOS App/TensorDash/TensorDash/Controllers/loginViewController.swift
+++ iOS App/TensorDash/TensorDash/Controllers/loginViewController.swift
@@ -13,8 +13,45 @@ class loginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        
+        //Hide Keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardwilchange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardwilchange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardwilchange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+    
     }
+    
+    // MARK: - Code below this is for hiding keyboard
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    func hideKeyboard(){
+        view.resignFirstResponder()
+    }
+    
+    @objc func keyboardwilchange(notification: Notification){
+        view.frame.origin.y = -210
+        
+    }
+    
+    //UITextFieldDeligate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        return true
+    }
+    
+    //Hide when touch outside keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        view.frame.origin.y = 0
+        
+    }
+
 
 }
