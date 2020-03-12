19cc82430978767f860f98465b7191bc7148fa61
pateldevang <devangpatel92@gmail.com>
2020-03-12 10:22:22 +0530

    Config firebase.

--- iOS App/TensorDash/TensorDash/Helpers/AppDelegate.swift
+++ iOS App/TensorDash/TensorDash/Helpers/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Configure Firebase
+        FirebaseApp.configure()
+        
         return true
     }
 
