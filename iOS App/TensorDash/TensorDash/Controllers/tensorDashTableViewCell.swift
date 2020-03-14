4fa4a0ed9273d2e58d3b0f76efedcfd939ea3f28
pateldevang <devangpatel92@gmail.com>
2020-03-14 23:35:08 +0530

    tensorDash tableView added.

---
+++ iOS App/TensorDash/TensorDash/Controllers/tensorDashTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  tensorDashTableViewCell.swift
+//  TensorDash
+//
+//  Created by Devang Patel on 14/03/20.
+//  Copyright © 2020 Tensor Dash. All rights reserved.
+//
+
+import UIKit
+
+class tensorDashTableViewCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS App/TensorDash/TensorDash/Controllers/tensorDashViewController.swift
@@ -0,0 +1,58 @@
+//
+//  tensorDashViewController.swift
+//  TensorDash
+//
+//  Created by Devang Patel on 14/03/20.
+//  Copyright © 2020 Tensor Dash. All rights reserved.
+//
+
+import UIKit
+
+class tensorDashViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var tensorTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+
+extension tensorDashViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    //MARK: - Table View datasource Method
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tensorTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! tensorDashTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    // Setting height for rows in tableView
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
+    }
+    
+    // Setting custom header heigth
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 17
+    }
+    
+    // Setting Custom headerView background color
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = #colorLiteral(red: 0.1882352941, green: 0.1882352941, blue: 0.1882352941, alpha: 1)
+        return headerView
+    }
+    
+   
+}
