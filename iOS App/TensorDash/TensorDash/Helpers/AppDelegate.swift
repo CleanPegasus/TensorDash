503c75cd78089e746fe30d41de2199da8dd6b1a4
pateldevang <devangpatel92@gmail.com>
2020-03-28 19:48:26 +0530

    Launch screen time increase.

--- iOS App/TensorDash/TensorDash/Helpers/AppDelegate.swift
+++ iOS App/TensorDash/TensorDash/Helpers/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        //Increase Lauch Time
+        Thread.sleep(forTimeInterval: 1.3)
+        
         // Configure Firebase
         FirebaseApp.configure()
         
