e6dd426c9821c8536c8a6327e7564e4cb7c4a375
CleanPegasus <arunk609@gmail.com>
2020-05-11 20:20:38 +0530

    Updated class management

--- tensordash/fastdash.py
+++ tensordash/fastdash.py
@@ -1,80 +1,11 @@
-import requests
 import json
 import fastai
 from fastai.torch_core import Any, Tensor, MetricsList, ifnone
 from fastai.basic_train import LearnerCallback, Learner
-import getpass
+from firebase_data import SendDataToFirebase
 import time
 
-class FirebaseError(Exception):
-    pass
-class SendDataToFirebase(object):
 
-    def __init__(self, key = None):
-        response = None
-    
-    def signin(self, email = None, password = None):
-        if(email == None):
-            email = input("Enter Email :")
-        if(email != None and password == None):
-            password = getpass.getpass("Enter Tensordash Password :")
-            
-        headers = {'Content-Type': 'application/json',}
-        params = (('key', 'AIzaSyDU4zqFpa92Jf64nYdgzT8u2oJfENn-2f8'),)
-        val = {
-            "email" : email,
-            "password": password,
-            "returnSecureToken": "true"
-        }
-        data = str(val)
-
-        try:
-            response = requests.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword', headers=headers, params=params, data=data)
-            output = response.json()
-            key = output['localId']
-            token = output['idToken']
-
-            auth_token = (('auth', token),)
-
-        except:
-            raise FirebaseError("Authentication Failed. Kindly create an account on the companion app")
-
-        return key, auth_token
-
-    def sendMessage(self, key = None, auth_token = None, params = None, ModelName = 'Sample Model'):
-        epoch, loss, val_loss, acc = params
-        
-        if(acc == None and val_loss == None):
-            data = '{"Epoch":' +  str(int(epoch)+1) + ', "Loss" :' + str(loss) + '}'
-        elif(acc == None):
-            data = '{"Epoch":' +  str(int(epoch)+1) + ', "Loss" :' + str(loss) + ', "Validation Loss":' + str(val_loss) + '}'
-        elif(val_loss == None):
-            data = '{"Epoch":' +  str(int(epoch)+1) + ', "Loss" :' + str(loss) + ', "Accuracy" :' + str(acc) + '}'
-        else:
-            data = '{"Epoch":' +  str(int(epoch)+1) + ', "Loss" :' + str(loss) + ', "Accuracy" :' + str(acc) + ', "Validation Loss":' + str(val_loss) + '}'
-
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data=data)
-
-    def updateRunningStatus(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{"Status" : "RUNNING"}'
-        response = requests.put('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
-
-        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Running"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
-
-    def updateCompletedStatus(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{"Status" : "COMPLETED"}'
-        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
-
-        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Completed"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
-
-    def crashAnalytics(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{"Status" : "CRASHED"}'
-        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
-
-        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Crashed"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
 
 SendData = SendDataToFirebase()
 
--- tensordash/tensordash.py
+++ tensordash/tensordash.py
@@ -1,83 +1,8 @@
-import requests
 import json
 import tensorflow as tf
-import getpass
 import time
+from firebase_data import SendDataToFirebase
 
-class FirebaseError(Exception):
-    pass
-class SendDataToFirebase(object):
-
-    def __init__(self, key = None):
-        response = None
-
-    def signin(self, email = None, password = None):
-        if(email == None):
-            email = input("Enter Email :")
-        if(email != None and password == None):
-            password = getpass.getpass("Enter Tensordash Password :")
-            
-        headers = {'Content-Type': 'application/json',}
-        params = (('key', 'AIzaSyDU4zqFpa92Jf64nYdgzT8u2oJfENn-2f8'),)
-        val = {
-            "email" : email,
-            "password": password,
-            "returnSecureToken": "true"
-        }
-        data = str(val)
-
-        try:
-            response = requests.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword', headers=headers, params=params, data=data)
-            output = response.json()
-            key = output['localId']
-            token = output['idToken']
-
-            auth_token = (('auth', token),)
-
-        except:
-            raise FirebaseError("Authentication Failed. Kindly create an account on the companion app")
-
-        return key, auth_token
-
-    def sendMessage(self, key = None, auth_token = None, params = None, ModelName = 'Sample Model'):
-        epoch, loss, acc, val_loss, val_acc = params
-
-        if(acc == None and val_loss == None):
-            data = '{"Epoch":' +  str(epoch+1) + ', "Loss" :' + str(loss) + '}'
-        elif(acc == None):
-            data = '{"Epoch":' +  str(epoch+1) + ', "Loss" :' + str(loss) + ', "Validation Loss":' + str(val_loss) + '}'
-        elif(val_loss == None):
-            data = '{"Epoch":' +  str(epoch+1) + ', "Loss" :' + str(loss) + ', "Accuracy" :' + str(acc) + '}'
-        else:
-            data = '{"Epoch":' +  str(epoch+1) + ', "Loss" :' + str(loss) + ', "Accuracy" :' + str(acc) + ', "Validation Loss":' + str(val_loss) + ', "Validation Accuracy" :' + str(val_acc) + '}'
-
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data=data)
-
-    def model_init(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{' + ModelName + ':' +  '"null"' + '}'
-        response = requests.put('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}.json'.format(key), params = auth_token, data = data)
-
-    def updateRunningStatus(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{"Status" : "RUNNING"}'
-        response = requests.put('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
-
-        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Running"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
-
-    def updateCompletedStatus(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{"Status" : "COMPLETED"}'
-        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
-
-        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Completed"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
-
-    def crashAnalytics(self, key = None, auth_token = None, ModelName = 'Sample Model'):
-        data = '{"Status" : "CRASHED"}'
-        response = requests.patch('https://cofeeshop-tensorflow.firebaseio.com/user_data/{}/{}.json'.format(key, ModelName), params = auth_token, data = data)
-
-
-        notif_data = '{"Key":' + '"' + str(key) + '"' + ', "Status" : "Crashed"}'
-        response = requests.post('https://cofeeshop-tensorflow.firebaseio.com/notification.json', params = auth_token, data = notif_data)
 
 SendData = SendDataToFirebase()
 class Tensordash(tf.keras.callbacks.Callback):
